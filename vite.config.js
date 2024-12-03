9f1a64965b903b65b9a45cd08e92eb0699ae6048
Viorel Andrei <vior3landr3i@yahoo.com>
2024-12-03 11:31:28 +0000

    Deploy site to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/News-Homepage/", // Setează calea relativă pentru GitHub Pages
   plugins: [react()],
-})
+});
